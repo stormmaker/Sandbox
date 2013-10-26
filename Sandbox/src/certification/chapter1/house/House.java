8e2540b08a10d13f413d77ec1d672dcad1baef66
Florian <hidden>
2013-10-27 01:01:45 +0200

    ch 1

---
+++ Sandbox/src/certification/chapter1/house/House.java
@@ -0,0 +1,11 @@
+package certification.chapter1.house;
+
+public class House {
+
+	static int i = 10;
+
+	public static void test() {
+		i = 20;
+	}
+
+}
---
+++ Sandbox/src/certification/chapter1/person/Person.java
@@ -0,0 +1,11 @@
+package certification.chapter1.person;
+
+import certification.chapter1.house.House;
+
+class Person extends House {
+
+	void test2() {
+		test();
+	}
+
+}
